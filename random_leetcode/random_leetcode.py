c634ccc67be9f8d4f851c11f08c0d0c56506cb42
2ashishs <2ashishs@gmail.com>
2024-02-25 21:03:29 +0530

    - Refactor code to move done file to $HOME and remove use of todo file - Updated README.md - Updated testpypi packaging

--- random_leetcode/random_leetcode.py
+++ random_leetcode/random_leetcode.py
@@ -7,7 +7,6 @@
 
 import os
 import random
-import shutil
 import subprocess
 
 
@@ -26,34 +25,30 @@ def write_urls_to_file(file_name, urls):
 
 
 def pick_random_and_open(urls_file, done_file):
-    # Read URLs from the input file
+    # Read URLs from the urls_file
     set_todo = read_urls_from_file(urls_file)
 
     if not set_todo:
         print("LeetCode ToDo List is empty.")
         return
 
-    # Read URLs from the done file
+    # Read URLs from the done_file
     set_done = read_urls_from_file(done_file)
 
-    # Remove URLs from set S that are already in set done
+    # Remove URLs from set_todo that are already in set_done
     set_todo -= set_done
 
     if not set_todo:
         print("Congratulations! All LeetCode Problems have been solved!!")
         return
 
-    # Pick a random URL from set S
+    # Pick a random URL from set_todo
     random_url = random.choice(list(set_todo))
 
     # Add the random URL to set done
     set_done.add(random_url)
 
-    # Remove the random URL from set_todo
-    set_todo.remove(random_url)
-
     # Write updated sets back to files
-    write_urls_to_file(urls_file, set_todo)
     write_urls_to_file(done_file, set_done)
 
     # Execute "open $random_url" in shell
@@ -62,31 +57,25 @@ def pick_random_and_open(urls_file, done_file):
     print(f"Open Random URL {random_url}.")
 
 
-def create_todo_file_and_done_file_if_not_exists(lc_master_file, lc_todo_file, lc_done_file):
-    if not os.path.exists(lc_todo_file):
+def create_done_file_if_not_exists(lc_top_questions_file, lc_done_file):
+    if not os.path.exists(lc_done_file):
         try:
-            # Create lc_todo_file by copying lc_master_file
-            shutil.copy(lc_master_file, lc_todo_file)
-            print(f"{lc_todo_file} created.")
-
             # Create lc_done_file as an empty file
             with open(lc_done_file, 'w'):
                 pass
             print(f"{lc_done_file} created.")
         except FileNotFoundError:
-            print(f"Error: {lc_master_file} not found.")
-    else:
-        pass
+            print(f"Error: {lc_top_questions_file} not found.")
 
 
 def main():
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    lc_master_file = os.path.join(script_dir, "lc_master.txt")
-    lc_todo_file = "lc_todo.txt"
-    lc_done_file = "lc_done.txt"
+    lc_top_questions_file = os.path.join(script_dir, ".lc_top_questions")
+    home_directory = os.path.expanduser("~")
+    lc_done_file = os.path.join(home_directory, ".lc_done")
 
-    create_todo_file_and_done_file_if_not_exists(lc_master_file, lc_todo_file, lc_done_file)
-    pick_random_and_open(lc_todo_file, lc_done_file)
+    create_done_file_if_not_exists(lc_top_questions_file, lc_done_file)
+    pick_random_and_open(lc_top_questions_file, lc_done_file)
 
 
 if __name__ == "__main__":
